6d5c761041c386543d4678c1395adeefbf1888f2
Sylsee <sylvio.poliart@gmail.com>
2021-09-13 02:42:37 +0200

    Philosophers

---
+++ includes/philosophers.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spoliart <spoliart@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/13 02:11:25 by spoliart          #+#    #+#             */
+/*   Updated: 2021/09/13 02:41:13 by spoliart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+void	philo();
+
+#endif
